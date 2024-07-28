b8885689e47a6d8e901c56ca654bc31ed8418179
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-07-28 16:28:42 +0700

    init

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,25 +1,28 @@
 // Navbar Fixed
 window.onscroll = function () {
-    const header = document.querySelector('header');
-    const myBtn = document.getElementById("myBtn")
-    const fixedNav = header.offsetTop;
+  const header = document.querySelector("header");
+  const myBtn = document.getElementById("myBtn");
+  const fixedNav = header.offsetTop;
 
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-        myBtn.style.display = "block";
-    } else {
-        header.classList.remove('navbar-fixed');
-        myBtn.style.display = "none";
-    }
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    myBtn.style.display = "block";
+  } else {
+    header.classList.remove("navbar-fixed");
+    myBtn.style.display = "none";
+  }
+};
+
+// Button Scroll To The Top
+function topFunction() {
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
 }
 
 // Hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
-})
-
-// Button Scroll To The Top
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
